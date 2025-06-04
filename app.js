4b92823964d31896e3855956031dc3f010516c31
Juan Vallejo <bagy45@gmail.com>
2025-06-03 21:11:45 -0400

    Implement email validation and error handling in newsletter sign-up form

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const dismissButton = document.getElementById('dismiss-message')
 const input = document.getElementById('email-input')
 const successMessage = document.getElementById('success-message-container')
 const mainContent = document.getElementById('main-content')
+const errorMessage = document.getElementById('error-message')
+const userEmail = document.getElementById('input-email')
 
 
 function dismissMessage () {
@@ -10,12 +12,21 @@ function dismissMessage () {
     mainContent.style.display = 'flex'
     location.reload();
 }
-
-
 function submitMessage() {
     mainContent.style.display = 'none'
     successMessage.style.display = 'flex'
+    userEmail.innerHTML = input.value
 }
-
-
-function validateInput() {}
+function validateEmail() {
+    let format = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    
+    if (input.value.match(format)) {
+        submitMessage()
+    } else toggleError();
+}
+function toggleError() {
+        input.style.backgroundColor = 'hsla(0, 100%, 50%, 0.3)'
+        input.style.color = 'red'
+        input.style.border = '1px solid red'
+        errorMessage.style.display = 'flex'
+    }
